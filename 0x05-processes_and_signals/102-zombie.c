e58e0089eb14972f65cc12414567a9156fd87d38
M9china <nqobilencaza2@gmail.com>
2023-10-30 03:35:52 -0700

    zombie process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void) {
+    while (1) {
+        sleep(1);
+    }
+    return 0;
+}
+
+int main(void) {
+    pid_t child_pid;
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        child_pid = fork();
+        if (child_pid == -1) {
+            perror("fork");
+            exit(1);
+        }
+        if (child_pid == 0) {
+            // Child process
+            printf("Zombie process created, PID: %d\n", getpid());
+            exit(0);
+        }
+    }
+
+    // Parent process
+    infinite_while(); // This will keep the parent running
+    return 0;
+}
+
